2341b4be9119eca23dc948445ebcd9b72b294a66
omeriko436 <talhayeniyurt@gmail.com>
2022-11-20 20:08:43 +0300

    MyBatProject1

---
+++ Project1/mainFile.bat
@@ -0,0 +1,7 @@
+@echo off
+@echo ©Sytem Deleter v1.0
+@echo This program just gonna delete your computer.
+pause>Null
+start warning.vbs
+pause>Null
+shutdown -s
---
+++ Project1/warning.vbs
@@ -0,0 +1,1 @@
+WScript.echo "This is an joke virus. Save your unsaved works. Your computer won't die. It will only asleep."
